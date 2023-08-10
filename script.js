f6e5c8263164736771b86b3967040a2ce17bf7c7
kevfracc <kevfracc@gmail.com>
2023-08-10 16:09:26 -0400

    forgot to commit idiot

--- script.js
+++ script.js
@@ -7,3 +7,38 @@ document.addEventListener("DOMContentLoaded", function () {
         menuToggle.classList.toggle("button-open");
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Retrieve the input values from Local Storage
+    const leftInput = document.getElementById("left-input");
+    const rightInput = document.getElementById("right-input");
+    const leftStoredInput = document.getElementById("left-stored-input");
+    const rightStoredInput = document.getElementById("right-stored-input");
+
+    leftInput.value = localStorage.getItem("leftInputValue") || "";
+    rightInput.value = localStorage.getItem("rightInputValue") || "";
+
+    // Display the stored input values below the input elements
+    leftStoredInput.textContent = leftInput.value;
+    rightStoredInput.textContent = rightInput.value;
+
+    // Store the input values to Local Storage when the inputs change
+    leftInput.addEventListener("input", function () {
+        localStorage.setItem("leftInputValue", leftInput.value);
+        leftStoredInput.textContent = leftInput.value;
+    });
+
+    rightInput.addEventListener("input", function () {
+        localStorage.setItem("rightInputValue", rightInput.value);
+        rightStoredInput.textContent = rightInput.value;
+    });
+});
+
+const loginButton = document.getElementById("login-button");
+    
+    loginButton.addEventListener("click", function () {
+        // Add your login functionality here
+        alert("Login button clicked!");
+    });
+
+    
