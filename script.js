18c723955605e9fee75e66cbc79c6b3619827b46
kevfracc <kevfracc@gmail.com>
2023-08-22 17:14:41 -0400

    find a better way than json saved files

--- script.js
+++ script.js
@@ -16,99 +16,83 @@
 ///percentage bar showing whos winning (60/40 left is winning, left's bar is across 60% of the screen)
 
 
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.getElementById("menu-toggle");
-    const menuBar = document.querySelector(".menu-bar");
-
-    menuToggle.addEventListener("click", function () {
-        menuBar.classList.toggle("menu-open");
-        menuToggle.classList.toggle("button-open");
-    });
-});
-
 function openURL() {
     window.open("https://www.cursor.so/pricing#faq");
 }
-document.addEventListener("DOMContentLoaded", function() {
-    var welcomePopup = document.getElementById("welcome-popup");
-    var okButton = document.getElementById("ok-button");
 
-    okButton.addEventListener("click", function() {
-        welcomePopup.style.display = "none";
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    var welcomePopup = document.getElementById("welcome-popup"); // other popup
-    var okButton = document.getElementById("fu-button");
-
-    okButton.addEventListener("click", function() {
-        welcomePopup.style.display = "none"; //something else displayed
-    });
+const loginButton = document.getElementById("login-button");
+    
+loginButton.addEventListener("click", function () {
+    // Add your login functionality here
+    alert("Login button clicked!");
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Retrieve the input values from Local Storage
-    const leftInput = document.getElementById("left-input");
-    const rightInput = document.getElementById("right-input");
-    const leftStoredInput = document.getElementById("left-stored-input");
-    const rightStoredInput = document.getElementById("right-stored-input");
-    const leftScore = document.getElementById("left-score");
-    const rightScore = document.getElementById("right-score");
- 
-    leftInput.value = localStorage.getItem("leftInputValue") || "";
-    rightInput.value = localStorage.getItem("rightInputValue") || "";
-    
-    // Display the stored input values below the input elements
-    leftStoredInput.textContent = leftInput.value;
-    rightStoredInput.textContent = rightInput.value;
-    leftScore.textContent = leftInput.value !== "" ? 1 : 0;
-    rightScore.textContent = rightInput.value !== "" ? 1 : 0;
+    const menuToggle = document.getElementById("menu-button");
+    const menuContainer = document.getElementById("menu-container");
+    const navTabs = document.querySelectorAll(".nav-tabs li a");
+
+    menuToggle.addEventListener("click", function () {
+        menuContainer.innerHTML = ""; // Clear existing links
+
+        if (!menuContainer.classList.contains("menu-open")) {
+            menuContainer.classList.add("menu-open");
+
+            navTabs.forEach(link => {
+                const navLinkElement = document.createElement("a");
+                navLinkElement.href = link.href;
+                navLinkElement.textContent = link.textContent;
+
+                menuContainer.appendChild(navLinkElement);
+            });
+        } else {
+            menuContainer.classList.remove("menu-open");
+        }
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var welcomePopup = document.getElementById("welcome-popup");
+    var okButton = document.getElementById("ok-button");
+    var fuButton = document.getElementById("fu-button");
+    var leftInput = document.getElementById("left-input");
+    var rightInput = document.getElementById("right-input");
+    var leftStoredInput = document.getElementById("left-stored-input");
+    var rightStoredInput = document.getElementById("right-stored-input");
+    var leftScore = document.getElementById("left-score");
+    var rightScore = document.getElementById("right-score");
+
+    // Clear the input values and scores
+    leftInput.value = "";
+    rightInput.value = "";
+    leftStoredInput.textContent = "";
+    rightStoredInput.textContent = "";
+    leftScore.textContent = "0";
+    rightScore.textContent = "0";
 
     // Store the input values to Local Storage when the inputs change
     leftInput.addEventListener("input", function () {
         localStorage.setItem("leftInputValue", leftInput.value);
         leftStoredInput.textContent = leftInput.value;
-        leftScore.textContent = leftInput.value !== "" ? 1 : 0;
     });
 
     rightInput.addEventListener("input", function () {
         localStorage.setItem("rightInputValue", rightInput.value);
         rightStoredInput.textContent = rightInput.value;
-        rightScore.textContent = rightInput.value !== "" ? 1 : 0;
+    });
+
+    okButton.addEventListener("click", function() {
+        var username = document.getElementById("username-input").value;
+        localStorage.setItem("username", username);
+        welcomePopup.style.display = "none";
+        leftScore.textContent = parseInt(leftScore.textContent) + 1;
+    });
+
+    fuButton.addEventListener("click", function() {
+        var username = document.getElementById("username-input").value;
+        localStorage.setItem("username", username);
+        welcomePopup.style.display = "none"; //something else displayed
+        rightScore.textContent = parseInt(rightScore.textContent) + 1;
     });
 });
-
-
-const loginButton = document.getElementById("login-button");
-    
-    loginButton.addEventListener("click", function () {
-        // Add your login functionality here
-        alert("Login button clicked!");
-    });
-    document.addEventListener("DOMContentLoaded", function () {
-        const menuToggle = document.getElementById("menu-button");
-        const menuContainer = document.getElementById("menu-container");
-        const navTabs = document.querySelectorAll(".nav-tabs li a");
-    
-        menuToggle.addEventListener("click", function () {
-            menuContainer.innerHTML = ""; // Clear existing links
-    
-            if (!menuContainer.classList.contains("menu-open")) {
-                menuContainer.classList.add("menu-open");
-    
-                navTabs.forEach(link => {
-                    const navLinkElement = document.createElement("a");
-                    navLinkElement.href = link.href;
-                    navLinkElement.textContent = link.textContent;
-    
-                    menuContainer.appendChild(navLinkElement);
-                });
-            } else {
-                menuContainer.classList.remove("menu-open");
-            }
-        });
-    });
-    
